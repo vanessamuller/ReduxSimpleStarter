67f16aa99efc5b832dd7239d3ff42bca515712d4
vanessamuller <vane9090@gmail.com>
2017-07-31 15:48:55 -0400

    added SearchBar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      term: ''
+    };
+  }
+
+  render() {
+    return <input onChange={(event)=> this.setState({term: event.target.value})} />
+  }
+
+
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD5uHqD_uj_Ev5bEhYOHKsAs3qVbf_x3U0';
+
 //Create a new component. This component should produce
 //some HTML
 
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 //Take this component's generated HTML and put it
 //on the page(in the DOM)
